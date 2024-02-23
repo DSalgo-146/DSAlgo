c0fc6ae59db0ef17cb6edc27bd510a2539dc00c8
girij <girij@GiriJay.home>
2024-02-23 12:09:49 -0500

    TestRunner Changes

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -3,10 +3,12 @@ package Runner;
 import org.junit.runner.RunWith;
 
 
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import io.cucumber.testng.*;
@@ -17,12 +19,15 @@ import io.cucumber.testng.*;
 				"html:target/results.html","json:target/cucumber.json","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
 		monochrome = false,
-		features = "src/test/resources/Features/Home.feature",
+		features = "src/test/resources/Features",
+		//features = {"src/test/resources/Features/home.feature","src/test/resources/Features/register.feature"},
+		//features = "src/test/resources/Features/Home.feature",
 		//features = "src/test/resources/Features/Register.feature",
 		//features = "src/test/resources/Features/Login.feature",
 		//features = "src/test/resources/Features/DataStructure.feature",
 		//features = "src/test/resources/Features/Tree.feature",
-		tags = "@tag1",		
+		//tags = "@tag1",		
+		//tags = "@tag1 or @logintag",
 		//features = "Features",
 		glue={"StepDefinition","Hooks"}
 		//glue={"StepDefinition"}
@@ -30,4 +35,10 @@ import io.cucumber.testng.*;
 
 public class TestRunner extends AbstractTestNGCucumberTests {
 
+	 @Override
+	    @DataProvider(parallel = true)
+	    public Object[][] scenarios() {
+	        return super.scenarios();
+	    }
+	 
 }
